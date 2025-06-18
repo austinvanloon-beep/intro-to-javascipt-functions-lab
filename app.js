22829c365a8035f1ae09238229b317558af25eac
Austin Vanloon <wic404learn@gmail.com>
2025-06-18 11:14:08 -0600

    exercises 4-7

--- app.js
+++ app.js
@@ -42,3 +42,187 @@ function isAdult(age) {
 
 
 console.log('Exercise 2 Result:', isAdult(21));
+
+
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(vowel) {
+    if (vowel === 'a' ||
+        vowel === 'e' ||
+        vowel === 'i' ||
+        vowel === 'o' ||
+        vowel === 'u')
+     {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+function generateEmail(emailname, domain) {
+    return emailname + "@" + domain;
+}
+
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+
+
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+
+function greetUser(name, timeofday) {
+    if (timeofday === 'morning') {
+        return "Good morning, " + name + "!";
+         } else if (timeofday === 'afternoon') {
+        return "Good afternoon, " + name + "!";
+    } else if (timeofday === 'evening') {
+        return "Good evening, " + name + "!";
+    } else {
+        return "Hello, " + name + "!";
+    }
+}
+
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+
+function maxOfThree(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else { 
+        return num3;
+    }
+}
+
+
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+
+
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+
+function calculateTip(bill, tip) {
+    return bill * (tip / 100);
+}
+
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+
+function convertTemperature(temperature, scale) {
+    return 
+}
+
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
